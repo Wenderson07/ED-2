e7f3079e0bb111e57905fa80369ad54673872aaf
Wenderson07 <Wenderson_pac@hotmail.com>
2017-10-14 12:34:50 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,15 @@
 package arvoreAVL;
-import java.io.*;                 
-import java.lang.Integer;
 
-public class main {
-	public static void main(String[] args) throws IOException{
-		arvore teste = arvore();
-		teste.inserir(10,teste);
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		arvore teste = new arvore(); //Crio novo elemento tipo arvore 
+		teste.inserir(15, teste.raiz); 	//insere elemento
+		teste.inserir(10, teste.raiz);
+		teste.inserir(2, teste.raiz);
+		teste.preOrdem(teste.raiz);
+		System.out.println("oi");
 	}
+
 }
