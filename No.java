1326f9b38c031d04f05ad9e4d1b941ebb42d679d
Wenderson07 <Wenderson_pac@hotmail.com>
2017-10-14 01:40:42 -0300

    Update No.java

--- No.java
+++ No.java
@@ -3,52 +3,15 @@ package arvoreAVL;
 
 //Essa Classe cria os nós
 public class No {
-	private int x;
-	int fatorDeBalanceamento;
-	private No pai;
-	private No esquerda;
-	private No direita;
-	private int altura; //pode ser removido
+	public int valor;
+	public No pai;
+	public No esquerda;
+	public No direita;
+	public int altura; //pode ser removido
 
 	
 	//Construtores 
-	public No pai() {
-		return this.pai;
-	}
-	
-	public No esquerda() {
-		return this.esquerda;
-	}
-	public No direita() {
-		return this.direita;
-	}
-	public int altura() {
-		return this.altura;
-	}
 
-	
-	public int valor() {
-		return this.x;
-	}
-}
-
-
-
-
-
-/*
-
-package arvoreAVL;
-
-
-//Essa Classe cria os nós
-public class No {
-	private int valor;
-	int fatorDeBalanceamento;
-	private No pai;
-	private No direita;
-	private No esquerda;
-	private int altura; //pode ser removido
 	
 	public No(int x) { //valor
 		pai = null;
@@ -57,6 +20,4 @@ public class No {
 		altura = 0;
 		valor = x;
 	}
-
 }
-*/
