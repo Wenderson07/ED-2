3a4aade1e8a2ce0a347a4c65e9d4d16dd9734578
Wenderson07 <Wenderson_pac@hotmail.com>
2017-10-14 01:41:20 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
 package arvoreAVL;
+import java.io.*;                 
+import java.lang.Integer;
 
 public class main {
-
+	public static void main(String[] args) throws IOException{
+		arvore teste = arvore();
+		teste.inserir(10,teste);
+	}
 }
-
